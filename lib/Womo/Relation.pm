bbae716e54901693ac4be8fff3d47ac692b929a7
Prog Rammer <prammer1@gmail.com>
2010-03-26 16:37:04 -0500

    compose Set::Relation (but just stub in many unimplemented methods) basic is_identical working, though far from ideal

--- lib/Womo/Relation.pm
+++ lib/Womo/Relation.pm
@@ -2,10 +2,8 @@
 package Womo::Relation;
 use Womo::Class;
 use Womo::Depot::Interface;
-use Set::Relation;
+use Set::Relation::V2;
 use Womo::Relation::Iterator;
-
-#with 'Set::Relation';
 
 has '_depot' => (
     init_arg => 'depot',
@@ -55,7 +53,7 @@ sub projection {
     my @attributes = @_;
     return (blessed $self)->new(
         _expr    => [ $self, 'projection', @attributes ],
-        _heading => [@attributes],
+        heading => [@attributes],
     );
 }
 
@@ -68,7 +66,7 @@ sub rename {
     my %new_map = ( %old_map, %map );
     return (blessed $self)->new(
         _expr    => [ $self, 'rename', %map ],
-        _heading => [ sort values %new_map ],
+        heading => [ sort values %new_map ],
     );
 }
 
@@ -77,9 +75,68 @@ sub restriction {
     my $expr = shift;
     return (blessed $self)->new(
         _expr    => [ $self, 'restriction', $expr ],
-        _heading => [ @{ $self->_heading } ],
+        heading => [ @{ $self->_heading } ],
     );
 }
+
+sub _as_v2 {
+    my $self = shift;
+    return Set::Relation::V2->new( $self->members );
+}
+
+sub is_identical {
+    my ( $self, $other ) = @_;
+    return 1 if ( $self == $other );
+    return $self->_as_v2->is_identical(
+        Set::Relation::V2->new( $other->members ) );
+}
+
+# TODO: to satisfy Set::Relation
+{
+    my $meta   = __PACKAGE__->meta;
+    my @method = (
+        'antijoin',               'attr',
+        'attr_names',             'body',
+        'cardinality',            'cardinality_per_group',
+        'classification',         'cmpl_group',
+        'cmpl_proj',              'cmpl_restr',
+        'cmpl_wrap',              'composition',
+        'count',                  'count_per_group',
+        'degree',                 'deletion',
+        'diff',                   'empty',
+        'exclusion',              'export_for_new',
+        'extension',              'group',
+        'has_attrs',              'has_key',
+        'has_member',             'heading',
+        'insertion',              'intersection',
+        'is_disjoint',            'is_empty',
+        'is_nullary',             'is_proper_subset',
+        'is_proper_superset',     'is_subset',
+        'is_superset',            'join',
+        'join_with_group',        'keys',
+        'limit',                  'limit_by_attr_names',
+        'map',                    'outer_join_with_exten',
+        'outer_join_with_group',  'outer_join_with_static_exten',
+        'outer_join_with_undefs', 'product',
+        'quotient',               'rank',
+        'rank_by_attr_names',     'restr_and_cmpl',
+        'semidiff',               'semijoin',
+        'semijoin_and_diff',      'slice',
+        'static_exten',           'static_subst',
+        'static_subst_in_restr',  'static_subst_in_semijoin',
+        'subst_in_restr',         'subst_in_semijoin',
+        'substitution',           'summary',
+        'symmetric_diff',         'tclose',
+        'ungroup',                'union',
+        'unwrap',                 'which',
+        'wrap'
+    );
+    for my $method (@method) {
+        $meta->add_method( $method => sub { die 'not implemented yet' } );
+    }
+}
+
+with 'Set::Relation';
 
 1;
 __END__
