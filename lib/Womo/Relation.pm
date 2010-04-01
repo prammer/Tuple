d73744c79f7a45bb49ba2862455fd87751495628
Prog Rammer <prammer1@gmail.com>
2010-04-01 16:22:48 -0500

    got more of test working restriction not working yet

--- lib/Womo/Relation.pm
+++ lib/Womo/Relation.pm
@@ -3,7 +3,8 @@ package Womo::Relation;
 use Womo::Class;
 use Womo::Depot::Interface;
 use Set::Relation::V2;
-use Womo::Relation::Iterator;
+require Womo::Relation::Iterator;
+require Womo::Relation::Operator;
 
 has '_depot' => (
     init_arg => 'depot',
@@ -19,7 +20,7 @@ has '_table_name' => (
 
 has '_expr' => (
     is  => 'ro',
-    isa => 'ArrayRef',
+    isa => 'Womo::Relation::Operator',
 );
 
 has '_heading' => (
@@ -34,7 +35,69 @@ has '_heading' => (
 sub _new_iterator {
     my $self = shift;
 
-    return Womo::Relation::Iterator->new( relation => $self );
+    return Womo::Relation::Iterator->new( relation => $self, sth => $self->_new_sth, );
+}
+
+sub _new_operator {
+    my $self = shift;
+    return Womo::Relation::Operator->new( @_, relation => $self, );
+}
+
+{
+    my $to_sql = {
+        'projection' => sub {
+            my @attributes = @_;
+            return
+                  "select distinct "
+                . join( ", ", @attributes )
+                . " from ("
+                . $_->_build_sql . ")",
+                ;
+        },
+        'rename' => sub {
+            my %map = @_;
+
+            # meh! this is repeated from sub rename above
+            my %old_map;
+            my @old_attr = @{ $_->_heading };
+            @old_map{@old_attr} = @old_attr;
+            my %new_map = ( %old_map, %map );
+
+            my $clause = join( ', ',
+                map { "$_ $new_map{$_}" } sort keys %new_map );
+            return "select $clause from ( " . $_->_build_sql . ')';
+        },
+        'restriction' => sub {
+            my $expr = shift;
+            return $_->_build_sql . " where " . $expr;
+        },
+    };
+
+    sub _build_sql {
+        my $self = shift;
+
+        my $r    = $self;
+        my $expr = $r->_expr;
+        if ($expr) {
+            my ( $inner, $oper, $args ) = map { $expr->$_ } qw(relation name args);
+            local $_ = $inner;
+            return $to_sql->{$oper}->(@$args);
+        }
+        my $table = $r->_table_name or die 'must have table_name';
+        my $col   = $r->_heading    or die 'must have heading';
+        return 'select distinct ' . join( ', ', @$col ) . " from $table";
+    }
+}
+
+sub _new_sth {
+    my $self = shift;
+
+    my $sql     = $self->_build_sql;
+#    print "\n$sql\n";
+    my $db_conn = $self->_depot->db_conn;
+    my $sth = $db_conn->run( sub { $_->prepare($sql); } );
+    $sth->execute or die;
+    return $sth;
 }
 
 sub members {
@@ -51,8 +114,11 @@ sub members {
 sub projection {
     my $self       = shift;
     my @attributes = @_;
-    return (blessed $self)->new(
-        _expr    => [ $self, 'projection', @attributes ],
+    return ( blessed $self)->new(
+        _expr => $self->_new_operator(
+            name => 'projection',
+            args => \@attributes,
+        ),
         heading => [@attributes],
     );
 }
@@ -64,8 +130,8 @@ sub rename {
     my @old_attr = @{ $self->_heading };
     @old_map{@old_attr} = @old_attr;
     my %new_map = ( %old_map, %map );
-    return (blessed $self)->new(
-        _expr    => [ $self, 'rename', %map ],
+    return ( blessed $self)->new(
+        _expr => $self->_new_operator( name => 'rename', args => [%map], ),
         heading => [ sort values %new_map ],
     );
 }
@@ -75,8 +141,11 @@ sub rename {
 sub restriction {
     my $self = shift;
     my $expr = shift;
-    return (blessed $self)->new(
-        _expr    => [ $self, 'restriction', $expr ],
+    return ( blessed $self)->new(
+        _expr => $self->_new_operator(
+            name => 'restriction',
+            args => [$expr],
+        ),
         heading => [ @{ $self->_heading } ],
     );
 }
--- lib/Womo/Relation/Iterator.pm
+++ lib/Womo/Relation/Iterator.pm
@@ -2,7 +2,7 @@
 package Womo::Relation::Iterator;
 
 use Womo::Class;
-use Womo::Relation;
+require Womo::Relation;
 
 with 'MooseX::Iterator::Role';
 
@@ -12,80 +12,12 @@ has 'relation' => (
     required => 1,
 );
 
-# TODO: not sure this belongs here
-has '_sql' => (
-    is       => 'ro',
-    isa      => 'Str',
-    required => 1,
-    lazy     => 1,
-    builder  => '_build_sql',
-);
-
-{
-    my $to_sql = {
-        'projection' => sub {
-            my @attributes = @_;
-            return
-                  "select distinct "
-                . join( ", ", @attributes )
-                . " from ("
-                . $_->_sql . ")",
-                ;
-        },
-        'rename' => sub {
-            my %map = @_;
-
-            # meh! this is repeated from sub rename above
-            my %old_map;
-            my @old_attr = @{ $_->_heading };
-            @old_map{@old_attr} = @old_attr;
-            my %new_map = ( %old_map, %map );
-
-            my $clause = join( ', ',
-                map { "$_ $new_map{$_}" } sort keys %new_map );
-            return "select $clause from ( " . $_->_sql . ')';
-        },
-        'restriction' => sub {
-            my $expr = shift;
-            return $_->_sql . " where " . $expr;
-        },
-    };
-
-    sub _build_sql {
-        my $self = shift;
-
-        # TODO: relying on private methods
-        my $r    = $self->relation;
-        my $expr = $r->_expr;
-        if ($expr) {
-            my ( $inner, $oper, @args ) = @$expr;
-            local $_ = $inner;
-            return $to_sql->{$oper}->(@args);
-        }
-        my $table = $r->_table_name or die 'must have table_name';
-        my $col   = $r->_heading    or die 'must have heading';
-        return 'select distinct ' . join( ', ', @$col ) . " from $table";
-    }
-}
-
 has '_sth' => (
+    init_arg => 'sth',
     is       => 'ro',
+#    isa => ???
     required => 1,
-    lazy     => 1,
-    builder  => '_new_sth',
 );
-
-sub _new_sth {
-    my $self = shift;
-
-    #TODO: uses private method
-    my $sql     = $self->_sql;
-#    print "\n$sql\n";
-    my $db_conn = $self->relation->_depot->db_conn;
-    my $sth = $db_conn->run( sub { $_->prepare($sql); } );
-    $sth->execute or die;
-    return $sth;
-}
 
 sub next {
     my $self = shift;
