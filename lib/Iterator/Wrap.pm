612395be849ac3409d39caa8527dbe6a712224e1
Prog Rammer <prammer1@gmail.com>
2010-07-23 22:57:44 -0500

    needed an array buffer like ::BulkWrap

--- lib/Iterator/Wrap.pm
+++ lib/Iterator/Wrap.pm
@@ -5,11 +5,12 @@ use Moose;
 use warnings FATAL => 'all';
 use namespace::autoclean;
 
+with 'Iterator';
+
 has 'iterator' => (
     is       => 'ro',
     does     => 'Iterator',
     required => 1,
-    handles  => [qw(has_next)],
 );
 
 has 'code' => (
@@ -18,23 +19,51 @@ has 'code' => (
     required => 1,
 );
 
-with 'Iterator';
+has '_buf' => (
+    init_arg  => undef,
+    is        => 'rw',
+    isa       => 'ArrayRef',
+    predicate => '_has_buf',
+    clearer   => '_clear_buf',
+);
 
 sub next {
     my $self = shift;
 
     return if !$self->has_next;
 
-    local $_ = $self->iterator->next;
-    my @values = $self->code->($_);
-    
-    if ($self->_has_buf) {
-        my $item = $self->_buf;
-        $self->_clear_buf;
-        return $item;
-    }
-    my $item = $self->_code->();
+    # we must have a non-empty array in _buf now
+    my $array = $self->_buf;
+    my $item  = shift @$array;
+    $self->_clear_buf if ( @$array == 0 );
     return $item;
+}
+
+sub has_next {
+    my $self = shift;
+
+    return 1 if $self->_has_buf;
+
+    # see if we can fill _buf with a non-empty array
+    my $it = $self->iterator;
+    my @array;
+    while (1) {
+        return if !$it->has_next;
+        local $_ = $it->next;
+        @array = $self->code->($_);
+        last if @array;
+    }
+    $self->_buf( \@array );
+    return 1;
+}
+
+sub peek {
+    my $self = shift;
+
+    return if !$self->has_next;
+
+    # we must have a non-empty array in _buf now
+    return $self->_buf->[0];
 }
 
 __PACKAGE__->meta->make_immutable;
